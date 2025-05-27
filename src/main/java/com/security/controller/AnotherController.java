c24e3013e0b50bf9eebad020383b48bc1d4aa2f2
Ritesh Kumar <ritkrs91@gmail.com>
2025-05-27 21:53:21 +0530

    another commit

--- src/main/java/com/security/controller/AnotherController.java
+++ src/main/java/com/security/controller/AnotherController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class AnotherController {
     @GetMapping("/api")
     public  String getDetails(){
-        return "Another Controller-V3";
+        return "Another Controller-20250527";
     }
 }
