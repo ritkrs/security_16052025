f193c6889a4ef84318b4da0a4ea15e133b6d2604
Ritesh Kumar <ritkrs91@gmail.com>
2025-06-03 20:32:08 +0530

    dir-refactoring

--- src/main/java/com/location_master/SecurityApplication.java
+++ src/main/java/com/location_master/SecurityApplication.java
@@ -1,4 +1,4 @@
-package com.security;
+package com.location_master;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/location_master/controller/AnotherController.java
+++ src/main/java/com/location_master/controller/AnotherController.java
@@ -1,4 +1,4 @@
-package com.security.controller;
+package com.location_master.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
